893cc5abef6481a07d900d217aacb9efd4841a2e
Lojacopsen <68278515+Lojacopsen@users.noreply.github.com>
2021-04-12 15:33:48 +0200

    Update updater.bat

--- updater.bat
+++ updater.bat
@@ -1,7 +1,6 @@
 @echo off
-start npm
-npm install git
+start npm install git
 start git clone https://github.com/rapid7/metasploit-framework
 cls
-echo msfconsole
+echo "updated!"
 rem require npm on cmd/windows powershell
