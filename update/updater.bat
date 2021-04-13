dee357bade158fb59f0658dd54f0ba44812f4705
Lojacopsen <68278515+Lojacopsen@users.noreply.github.com>
2021-04-13 17:56:18 +0200

    Update updater.bat

--- update/updater.bat
+++ update/updater.bat
@@ -3,8 +3,8 @@ start npm install --save npm-git-install
 ping localhost -n 2 >null
 start git clone https://github.com/rapid7/metasploit-framework
 cls
-echo "updating..."
+echo updating...
 ping localhost -n 6 >null
 color 04 
-echo updated
+echo updated!
 rem require npm on cmd/windows powershell
